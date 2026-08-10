7e654292353df1bad0305a5ba6437bf6da75dd8d
song892 <song892@users.noreply.github.com>
2026-08-10 05:03:19 +0000

    deploy: 5bbeaa44e8473fef79a6c79b75d6023c852f8e16

--- menu__system_8c.js
+++ menu__system_8c.js
@@ -2,7 +2,7 @@ var menu__system_8c =
 [
     [ "MS_decrementValue", "menu__system_8c.html#a28391906e0d6d059553293e1e2e6e7f4", null ],
     [ "MS_incrementValue", "menu__system_8c.html#ad29034f2b375212cc2bb9d4ef615583d", null ],
-    [ "MS_listGetSelected", "menu__system_8c.html#a23c0189c067cb5dfd0a1ab4514e0ecc0", null ],
+    [ "MS_listGetSelected", "menu__system_8c.html#a6451ded48042482563c062a315fb064e", null ],
     [ "MS_moveDown", "menu__system_8c.html#a7e670c0b8fc296a5cb8e5e5f340e508c", null ],
     [ "MS_moveUp", "menu__system_8c.html#abb6e83f43c566270750060807da99fa4", null ],
     [ "MS_refreshPage", "menu__system_8c.html#aaaa431be258445ad9f1278c488a3f502", null ],
