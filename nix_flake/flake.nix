8929eb123b194c4b006b5900737ac3df50118beb
LelsersLasers <millankumar@gmail.com>
2026-08-14 13:27:24 -0700

    update nix flake for unit test things
    manually set cc/cxx to override to GCC. I know clang works but I like gcc more

--- nix_flake/flake.nix
+++ nix_flake/flake.nix
@@ -32,6 +32,9 @@
             clang
             libclang
             clang-tools
+            lcov
+            gtest
+            cppcheck
 
             gcc-arm-embedded
             openocd
@@ -68,9 +71,14 @@
               pkgs.dbus
             ]}:$LD_LIBRARY_PATH
 
+            export CC="${pkgs.gcc}/bin/gcc"
+            export CXX="${pkgs.gcc}/bin/g++"
+
             echo "Purdue Electric Racing development environment"
             echo "  CMake:      $(cmake --version | head -n1)"
             echo "  Ninja:      $(ninja --version)"
+            echo "  GCC:        $($CC --version | head -n1)"
+            echo "  G++:        $($CXX --version | head -n1)"
             echo "  ARM GCC:    $(arm-none-eabi-gcc --version | head -n1)"
             echo "  OpenOCD:    $(openocd --version 2>&1 | head -n1)"
             echo "  Python:     $(python --version)"
