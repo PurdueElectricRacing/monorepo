d4f3efef552e7ea67844dc0258439533db30e80a
irving <irvingw@purdue.edu>
2026-08-14 00:38:06 -0400

    dont care

--- firmware/firmware_build.py
+++ firmware/firmware_build.py
@@ -129,6 +129,7 @@ def run_cppcheck():
         "--file-filter=*/firmware/can_library/*",
         "--suppress=*:*/external/*",
         "--suppress=*:*/source/torque_vector/vcu/*",
+        "--suppress=*:*/common/phal_F4/*",
         "--inline-suppr",
         "--quiet",
         f"-j{os.cpu_count() or 1}",
