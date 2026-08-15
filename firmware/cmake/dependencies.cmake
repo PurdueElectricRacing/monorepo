5b68fb597de52499f29d01fcb199f273eeb58c8d
LelsersLasers <millankumar@gmail.com>
2026-08-14 21:31:18 -0700

    Improve names in deps cmake

--- firmware/cmake/dependencies.cmake
+++ firmware/cmake/dependencies.cmake
@@ -1,7 +1,7 @@
 # Third-party firmware dependency setup (CMSIS Core, CMSIS device headers, FreeRTOS kernel)
 
-set(PER_CMSIS_CORE_DIR "${CMAKE_SOURCE_DIR}/external/CMSIS_6/CMSIS/Core/Include")
-set(PER_FREERTOS_DIR "${CMAKE_SOURCE_DIR}/external/FreeRTOS-Kernel")
+set(CMSIS_CORE_DIR "${CMAKE_SOURCE_DIR}/external/CMSIS_6/CMSIS/Core/Include")
+set(FREERTOS_KERNEL_DIR "${CMAKE_SOURCE_DIR}/external/FreeRTOS-Kernel")
 
 function(per_require_path path description)
     if(NOT EXISTS "${path}")
@@ -15,7 +15,7 @@ function(per_add_stm32_cmsis_target target family device device_dir)
     string(TOLOWER "${family}" family_lower)
     string(TOLOWER "${device}" device_lower)
 
-    per_require_path("${PER_CMSIS_CORE_DIR}/core_cm4.h" "CMSIS Core")
+    per_require_path("${CMSIS_CORE_DIR}/core_cm4.h" "CMSIS Core")
     per_require_path("${device_dir}/Include/${device_lower}.h" "${device} CMSIS device headers")
     per_require_path("${device_dir}/Source/Templates/system_${family_lower}.c" "${family} system source")
     per_require_path("${device_dir}/Source/Templates/gcc/startup_${device_lower}.s" "${device} startup source")
@@ -32,28 +32,28 @@ function(per_add_stm32_cmsis_target target family device device_dir)
 
     target_compile_definitions(${target} PUBLIC ${family} ${device})
     target_include_directories(${target} PUBLIC
-        "${PER_CMSIS_CORE_DIR}"
+        "${CMSIS_CORE_DIR}"
         "${device_dir}/Include"
     )
 endfunction()
 
 function(per_add_freertos_target target cmsis_target)
-    per_require_path("${PER_FREERTOS_DIR}/include/FreeRTOS.h" "FreeRTOS Kernel")
-    per_require_path("${PER_FREERTOS_DIR}/portable/GCC/ARM_CM4F/port.c" "FreeRTOS Cortex-M4F port")
+    per_require_path("${FREERTOS_KERNEL_DIR}/include/FreeRTOS.h" "FreeRTOS Kernel")
+    per_require_path("${FREERTOS_KERNEL_DIR}/portable/GCC/ARM_CM4F/port.c" "FreeRTOS Cortex-M4F port")
 
     add_library(${target} STATIC
-        "${PER_FREERTOS_DIR}/list.c"
-        "${PER_FREERTOS_DIR}/queue.c"
-        "${PER_FREERTOS_DIR}/tasks.c"
-        "${PER_FREERTOS_DIR}/timers.c"
-        "${PER_FREERTOS_DIR}/event_groups.c"
-        "${PER_FREERTOS_DIR}/stream_buffer.c"
-        "${PER_FREERTOS_DIR}/portable/GCC/ARM_CM4F/port.c"
+        "${FREERTOS_KERNEL_DIR}/list.c"
+        "${FREERTOS_KERNEL_DIR}/queue.c"
+        "${FREERTOS_KERNEL_DIR}/tasks.c"
+        "${FREERTOS_KERNEL_DIR}/timers.c"
+        "${FREERTOS_KERNEL_DIR}/event_groups.c"
+        "${FREERTOS_KERNEL_DIR}/stream_buffer.c"
+        "${FREERTOS_KERNEL_DIR}/portable/GCC/ARM_CM4F/port.c"
     )
 
     target_include_directories(${target} PUBLIC
-        "${PER_FREERTOS_DIR}/include"
-        "${PER_FREERTOS_DIR}/portable/GCC/ARM_CM4F"
+        "${FREERTOS_KERNEL_DIR}/include"
+        "${FREERTOS_KERNEL_DIR}/portable/GCC/ARM_CM4F"
         "${CMAKE_SOURCE_DIR}/common/rtos"
     )
     target_link_libraries(${target} PUBLIC ${cmsis_target})
