3211dae423b33bcb12610fbb6a6623d8272535f0
aaryapatnaik <66499097+aaryapatnaik@users.noreply.github.com>
2026-08-22 02:40:12 -0400

    ran cargo fmt

--- daqapp/src/widget_ids.rs
+++ daqapp/src/widget_ids.rs
@@ -17,14 +17,14 @@ impl WidgetIds {
     pub fn next(&mut self, kind: widget_constructor::WidgetConstructor) -> usize {
         let disc = if matches!(kind, widget_constructor::WidgetConstructor::ScopeEmpty) {
             std::mem::discriminant(&widget_constructor::WidgetConstructor::Scope {
-            msg_id: 0,
-            msg_name: String::new(),
-            signal_name: String::new(),
-        })
+                msg_id: 0,
+                msg_name: String::new(),
+                signal_name: String::new(),
+            })
         } else {
-        std::mem::discriminant(&kind)
+            std::mem::discriminant(&kind)
         };
-        
+
         let counter = self.counters.entry(disc).or_insert(1);
         let id = *counter;
         *counter += 1;
