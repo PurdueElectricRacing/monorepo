f6ea37f7f4618b88a823cb392a81171f39f0040a
irving <irvingw@purdue.edu>
2026-08-23 16:37:25 -0400

    name

--- firmware/common/utils/orientation.h
+++ firmware/common/utils/orientation.h
@@ -19,7 +19,6 @@ typedef struct {
  * @return a 3x3 DCM representing the rotation
  */
 static inline matrix3x3_t DCM_from_euler(const euler_angles_t angles) {
-    matrix3x3_t rot;
     float cos_roll  = cosf(angles.roll);
     float sin_roll  = sinf(angles.roll);
     float cos_pitch = cosf(angles.pitch);
@@ -29,19 +28,20 @@ static inline matrix3x3_t DCM_from_euler(const euler_angles_t angles) {
 
     // ZYX Euler sequence (Yaw -> Pitch -> Roll)
     // R = Rz(yaw) * Ry(pitch) * Rx(roll)
-    rot.data[0][0] = cos_pitch * cos_yaw;
-    rot.data[0][1] = cos_yaw * sin_pitch * sin_roll - cos_roll * sin_yaw;
-    rot.data[0][2] = sin_roll * sin_yaw + cos_roll * cos_yaw * sin_pitch;
+    matrix3x3_t dcm;
+    dcm.data[0][0] = cos_pitch * cos_yaw;
+    dcm.data[0][1] = cos_yaw * sin_pitch * sin_roll - cos_roll * sin_yaw;
+    dcm.data[0][2] = sin_roll * sin_yaw + cos_roll * cos_yaw * sin_pitch;
 
-    rot.data[1][0] = cos_pitch * sin_yaw;
-    rot.data[1][1] = cos_roll * cos_yaw + sin_roll * sin_pitch * sin_yaw;
-    rot.data[1][2] = cos_roll * sin_pitch * sin_yaw - cos_yaw * sin_roll;
+    dcm.data[1][0] = cos_pitch * sin_yaw;
+    dcm.data[1][1] = cos_roll * cos_yaw + sin_roll * sin_pitch * sin_yaw;
+    dcm.data[1][2] = cos_roll * sin_pitch * sin_yaw - cos_yaw * sin_roll;
 
-    rot.data[2][0] = -sin_pitch;
-    rot.data[2][1] = cos_pitch * sin_roll;
-    rot.data[2][2] = cos_pitch * cos_roll;
+    dcm.data[2][0] = -sin_pitch;
+    dcm.data[2][1] = cos_pitch * sin_roll;
+    dcm.data[2][2] = cos_pitch * cos_roll;
 
-    return rot;
+    return dcm;
 }
 
 #endif // ORIENTATION_H
