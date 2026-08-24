78f586768454bc0b4fb1e7f13b0de687620e440a
Ronak Jain <ronak@ronakpjain.com>
2026-08-24 19:31:59 -0400

    Fix bootloader CMSIS include

--- firmware/common/bootloader/bootloader_common.c
+++ firmware/common/bootloader/bootloader_common.c
@@ -6,7 +6,7 @@
 
 #include "common/bootloader/bootloader_common.h"
 
-#include "common/phal_G4/phal_G4.h"
+#include "stm32g474xx.h"
 
 /* Survives NVIC_SystemReset() without being initialized by startup code. */
 __attribute__((section(".noinit")))
