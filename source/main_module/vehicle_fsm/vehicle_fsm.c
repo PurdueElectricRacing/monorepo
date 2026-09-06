6b03d1c3b1f32d8dc82c55e37d2d4fd6cab0f2fa
irving <irvingw@purdue.edu>
2026-09-06 19:30:14 -0400

    bump regen gains

--- source/main_module/vehicle_fsm/vehicle_fsm.c
+++ source/main_module/vehicle_fsm/vehicle_fsm.c
@@ -39,8 +39,8 @@ static torque_request_t zero_torque_request() {
 }
 
 static torque_request_t direct_mapped_regen() {
-    // Map brake [0, 100] to torque [0, -50]
-    int16_t regen_torque = can_data.pedals.regen * -0.5f;
+    // Map regen [0, 100] to torque [0, -100]
+    int16_t regen_torque = can_data.pedals.regen * -1.0f;
 
     torque_request_t torque_request = {
         .front_left  = regen_torque,
@@ -76,7 +76,6 @@ static void update_torque_request() {
     }
 
     // regen guards
-    bool is_regen_commanded = (can_data.pedals.regen) > 5;
     int16_t min_wheelspeed = MINOF(
         g_car.front_right.crit->AMK_ActualSpeed,
         g_car.front_left.crit->AMK_ActualSpeed,
@@ -88,7 +87,8 @@ static void update_torque_request() {
     float pack_voltage = can_data.pack_stats.pack_voltage * UNPACK_COEFF_PACK_STATS_PACK_VOLTAGE;
     bool is_pack_voltage_fresh = !can_data.pack_stats.is_stale();
 
-    bool is_vehicle_speed_high = vehicle_speed_mph > 5.0f;
+    bool is_regen_commanded = can_data.pedals.regen > 5;
+    bool is_vehicle_speed_high = vehicle_speed_mph > 2.0f;
     bool is_pack_low_enough = pack_voltage < 480.0f;
     bool is_regen_allowed = is_regen_commanded && is_vehicle_speed_high && is_pack_low_enough && is_pack_voltage_fresh;
 
