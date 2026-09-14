ed560ff86b724622a861939284b28abbc5b3ab2d
Danny <179722984+DanielProano@users.noreply.github.com>
2026-09-13 23:58:43 -0400

    config route

--- firmware/generators/core/config.py
+++ firmware/generators/core/config.py
@@ -21,3 +21,7 @@ NODE_CONFIG_DIR          = CONFIG_DIR / "nodes"
 EXTERNAL_NODE_CONFIG_DIR = CONFIG_DIR / "external_nodes"
 COMMON_TYPES_CONFIG_PATH = SYSTEM_CONFIG_DIR / "common_types.json"
 BUS_CONFIG_PATH          = SYSTEM_CONFIG_DIR / "bus_configs.json"
+
+UNIT_CONFIG_DIR            = CONFIG_DIR / "units"
+BASE_TYPES_CONFIG_PATH     = UNIT_CONFIG_DIR / "base_types.json"
+COMPOUND_TYPES_CONFIG_PATH = UNIT_CONFIG_DIR / "compound_types.json"
