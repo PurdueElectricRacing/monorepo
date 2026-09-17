f95ba0bce2233b99350f6b9ce2e88ce0dba213ab
irving <irvingw@purdue.edu>
2026-09-16 22:03:57 -0400

    move generators to top level

--- generators/core/config.py
+++ generators/core/config.py
@@ -8,7 +8,8 @@ from pathlib import Path
 
 
 GENERATOR_DIR   = Path(__file__).resolve().parents[1]
-CAN_LIBRARY_DIR = GENERATOR_DIR.parent / "can_library"
+REPOSITORY_DIR  = GENERATOR_DIR.parent
+CAN_LIBRARY_DIR = REPOSITORY_DIR / "firmware" / "can_library"
 
 CONFIG_DIR         = GENERATOR_DIR / "configs"
 CAN_TEMPLATE_DIR   = GENERATOR_DIR / "canpiler" / "templates"
