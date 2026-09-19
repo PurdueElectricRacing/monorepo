8a818a3c1660803986ba2cef9014fe745fe1a67e
Ronak Jain <ronak@ronakpjain.com>
2026-09-19 00:51:48 -0400

    Remove stale CAN bus selection

--- daqapp/src/settings.rs
+++ daqapp/src/settings.rs
@@ -10,8 +10,6 @@ pub struct Settings {
     pub dbc_path: Option<std::path::PathBuf>,
     pub selected_source: Option<connection::ConnectionSource>,
     pub selected_speed: connection::CanBusSpeed,
-    #[serde(default)]
-    pub selected_bus: connection::CanBus,
     pub udp_port: u16,
     pub theme: theme::ThemeSelection,
     pub pixels_per_point: Option<f32>,
@@ -25,7 +23,6 @@ impl Default for Settings {
             dbc_path: None,
             selected_source: None,
             selected_speed: DEFAULT_CAN_SPEED,
-            selected_bus: connection::CanBus::Vcan,
             udp_port: DEFAULT_UDP_PORT,
             theme: theme::ThemeSelection::Default,
             pixels_per_point: None,
