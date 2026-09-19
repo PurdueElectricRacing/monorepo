5b3d20cdbc2729e97671d4cf9323c7c6233e5c45
Ronak Jain <ronak@ronakpjain.com>
2026-09-19 01:30:51 -0400

    fix daqapp themes, and settings.json discovery

--- daq/daqapp/src/ui/sidebar.rs
+++ daq/daqapp/src/ui/sidebar.rs
@@ -38,7 +38,7 @@ pub fn show(app: &mut app::DAQApp, ctx: &egui::Context) {
             let theme_label = format!("🎨 Theme: {}", app.theme_selection.get_name());
 
             if ui.button(theme_label).clicked() {
-                app.toggle_theme();
+                app.toggle_theme(ui.ctx());
                 app.save_settings();
             }
 
