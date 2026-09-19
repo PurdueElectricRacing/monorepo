5f339095ea90d625c85e8305fefbeb284932db2f
Patrick McNaughton <65691549+PatMcN7@users.noreply.github.com>
2026-09-19 17:28:22 -0400

    write system and task structs

--- firmware/common/diagnostics/diagnostics.c
+++ firmware/common/diagnostics/diagnostics.c
@@ -1,11 +1,22 @@
 #include "diagnostics.h"
+
 #include <string.h>
+
 #include "common/rtos/rtos.h"
 
 static TaskStatus_t raw_tasks[DIAGNOSTICS_MAX_TASKS];
 static diagnostics_snapshot_t latest_snapshot;
 static diagnostics_snapshot_t working_snapshot;
-static bool snapshot_ready = false;
+static uint32_t total_rutime;
+static uint32_t previous_total_runtime;
+static UBaseType_t previous_task_count;
+
+typedef struct {
+    UBaseType_t task_id;
+    uint32_t runtime;
+} diagnostics_prev_task_time_t;
+
+static diagnostics_prev_task_time_t prev_task_times[DIAGNOSTICS_MAX_TASKS];
 
 static void diagnostics_periodic(void);
 
@@ -17,32 +28,57 @@ void diagnostics_start(void) {
 }
 
 static void diagnostics_periodic(void) {
-    configRUN_TIME_COUNTER_TYPE total_runtime = 0;
-    UBaseType_t count = uxTaskGetSystemState(raw_tasks, DIAGNOSTICS_MAX_TASKS, &total_runtime);
-
-    working_snapshot.task_count = (uint32_t)count;
-    working_snapshot.timestamp_ms = (uint32_t) xTaskGetTickCount();
+    UBaseType_t count    = uxTaskGetSystemState(raw_tasks, DIAGNOSTICS_MAX_TASKS, &total_rutime);
+    // total time delta inbetween diagnostics_periodic function calls
+    uint32_t total_delta = total_rutime - previous_total_runtime;
+    uint32_t task_delta;
+    TaskHandle_t idle_task = xTaskGetIdleTaskHandle();
+    working_snapshot.task_count             = (uint32_t)count;
+    working_snapshot.timestamp_ms           = (uint32_t)xTaskGetTickCount();
     working_snapshot.task_capacity_exceeded = false;
-
     for (UBaseType_t i = 0; i < count; i++) {
-      TaskStatus_t *source = &raw_tasks[i];
-      diagnostics_task_t *destination = &working_snapshot.tasks[i];
+        TaskStatus_t *source_task            = &raw_tasks[i];
+        diagnostics_task_t *destination_task = &working_snapshot.tasks[i];
+        // zero out destination task
+        *destination_task = (diagnostics_task_t) {0};
+        
+        destination_task->task_id = source_task->xTaskNumber;
+        strcpy(destination_task->name, source_task->pcTaskName);
+        destination_task->stack_min_free_bytes =
+            source_task->usStackHighWaterMark * sizeof(StackType_t);
+        destination_task->state = source_task->eCurrentState;
 
-      *destination = (diagnostics_task_t){0};
+        // Calculate per-task CPU usage
+        for (UBaseType_t j = 0; j < previous_task_count; j++) {
+            if (prev_task_times[j].task_id == source_task->xTaskNumber && total_delta > 0
+                && (task_delta = source_task->ulRunTimeCounter - prev_task_times[j].runtime)
+                    <= total_delta) {
+                destination_task->cpu_usage_percent = 100.0f
+                    * (float)(task_delta)
+                    / (float)(total_delta);
+                destination_task->cpu_usage_valid = true;
+                break;
+            }
+        }
 
-      destination->task_id = (uint32_t)source->xTaskNumber;
-      destination->state = source->eCurrentState;
-
-      destination->stack_min_free_bytes =
-          (uint32_t)source->usStackHighWaterMark
-          * (uint32_t)sizeof(StackType_t);
-
-      strncpy(
-          destination->name,
-          source->pcTaskName,
-          sizeof(destination->name) - 1
-      );
-
-      destination->cpu_usage_valid = false;
+        working_snapshot.sample_count++;
+        working_snapshot.cpu_usage_percent = 0.0f;
+        working_snapshot.cpu_usage_valid = false;
+        // if you have more than the max tasks, uxTaskGetSystemState fails and returns 0
+        working_snapshot.task_capacity_exceeded = (count == 0);
+        // Calculate overall CPU usage
+        if (source_task->xHandle == idle_task && destination_task->cpu_usage_valid){
+            working_snapshot.cpu_usage_percent = 100.0f - destination_task->cpu_usage_percent;
+            working_snapshot.cpu_usage_valid = true;
+        }
     }
+    // update previous task run times
+    for (UBaseType_t i = 0; i < count; i++) {
+        prev_task_times[i].task_id = raw_tasks[i].xTaskNumber;
+        prev_task_times[i].runtime = raw_tasks[i].ulRunTimeCounter;
+    }
+
+    previous_total_runtime = total_rutime;
+    previous_task_count    = count;
+    latest_snapshot = working_snapshot;
 }
--- firmware/common/rtos/FreeRTOSConfig.h
+++ firmware/common/rtos/FreeRTOSConfig.h
@@ -128,6 +128,7 @@ to exclude the API function. */
 #define INCLUDE_uxTaskGetStackHighWaterMark 1
 #define INCLUDE_eTaskGetState 1
 #define INCLUDE_xTimerPendFunctionCall 1
+#define INCLUDE_xTaskGetIdleTaskHandle 1
 
 #define INCLUDE_xSemaphoreGetMutexHolder 1
 
