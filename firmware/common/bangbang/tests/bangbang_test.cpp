c43c99f6853812d1383e4ce690546019b65b729a
IAmRice33 <kyle.yang7728@gmail.com>
2026-09-20 22:11:11 -0400

    Added folder and base files for bang-bang controller update unit tests

---
+++ firmware/common/bangbang/tests/bangbang_test.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file bangbang_test.cpp
+ * @brief Bang-bang controller update function unit tests
+ * 
+ * @author Kyle Yang (yang3122@purdue.edu)
+ */
+
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "bangbang.h"
+}
