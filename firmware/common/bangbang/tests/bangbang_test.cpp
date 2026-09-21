7aa089a1326833e55b58f1b8706b02df6a3bf211
Kyle Yang <kyle.yang7728@gmail.com>
2026-09-21 15:19:05 -0400

    Setup initialization for gtests with a generic bang-bang controller and commented ideas for testing

--- firmware/common/bangbang/tests/bangbang_test.cpp
+++ firmware/common/bangbang/tests/bangbang_test.cpp
@@ -5,8 +5,46 @@
  * @author Kyle Yang (yang3122@purdue.edu)
  */
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
 extern "C" {
-#include "bangbang.h"
+    #include "bangbang.h"
 }
+
+namespace TestCallbacks {
+    void onCallback() {
+
+    }
+
+    void offCallback() {
+
+    }
+}
+class BangbangTest: public testing::Test {
+  protected:
+    void SetUp() override {
+        controller.upper_bound = 50.0f;
+        controller.lower_bound = 25.0f;
+        controller.on_func = TestCallbacks::onCallback;
+        controller.off_func = TestCallbacks::offCallback;
+        controller.last_switch_ms = 0;
+        controller.min_switch_interval = 1000;
+        controller.is_on = false;
+    }
+
+    bangbang_t controller;
+};
+
+// Create a bang-bang controller object
+
+// Test that last saved ms works
+
+// Test upper bounds
+
+// Test lower bounds
+
+// Test that min interval works (quick switching)
+
+// Test edge cases for upper and lower bounds
+
+// Test null pointers for functions
