6bd5caaa5f410e4042191b3d456181ed36f61cae
Patrick McNaughton <65691549+PatMcN7@users.noreply.github.com>
2026-09-22 20:07:47 -0400

    more docs

--- firmware/common/diagnostics/diagnostics.h
+++ firmware/common/diagnostics/diagnostics.h
@@ -14,39 +14,51 @@
 #include "task.h"
 
 #define DIAGNOSTICS_MAX_TASKS 16
-
+/** Define diagnostics task with FreeRTOS */
 #define DEFINE_DIAGNOSTICS_TASK() \
     RTOS_DEFINE_TASK(diagnostics_periodic, 1000, TASK_PRIORITY_LOW, STACK_1024)
+/** Start previously defined diagnostic task */
 #define START_DIAGNOSTICS_TASK() RTOS_START_TASK(diagnostics_periodic)
 
+/** Snapshot of a task. Contains id, state, name, cpu usage %, if the cpu usage is valid, stack HWM. */
 typedef struct {
     uint32_t task_id;
     char name[configMAX_TASK_NAME_LEN];
     eTaskState state;
 
-    uint32_t stack_min_free_bytes;
+    uint32_t stack_min_free_bytes; /** Fewest bytes that have ever been available on the stack. */
 
     float cpu_usage_percent;
-    bool cpu_usage_valid;
+    bool cpu_usage_valid; /** True if usage can be computed. */
 } diagnostics_task_t;
 
+/** Snapshot of the CPU. Contains system and task stats.  */
 typedef struct {
     uint32_t sample_count;
     uint32_t timestamp_ms;
     uint32_t task_count;
 
     float cpu_usage_percent;
-    bool cpu_usage_valid;
-    bool task_capacity_exceeded;
+    bool cpu_usage_valid; /** True if usage can be computed. */
+    bool
+        task_capacity_exceeded; /** True if there are more than DIAGNOSTICS_MAX_TASKS. If true no diagnostics data will be recorded. */
 
     diagnostics_task_t tasks[DIAGNOSTICS_MAX_TASKS];
 } diagnostics_snapshot_t;
 
+/** Contains the id of a task, and its last recorded runtime.
+*   Used for calculating how long a task has been running.
+*/
 typedef struct {
     UBaseType_t task_id;
     uint32_t runtime;
 } diagnostics_prev_task_time_t;
 
+/** @brief Contains all globals used in diagnostics.c 
+* 
+*   This struct is filled out by diagnostics_periodic()
+*   Includes all needed variables for performing profiling.
+*/
 typedef struct {
     TaskStatus_t raw_tasks[DIAGNOSTICS_MAX_TASKS];
     diagnostics_snapshot_t working_snapshot;
@@ -56,6 +68,11 @@ typedef struct {
     diagnostics_prev_task_time_t prev_task_times[DIAGNOSTICS_MAX_TASKS];
 } diagnostics_context_t;
 
+/**
+* @brief Periodic function that collects profiling data
+*
+*  Fills out a diagnostics_context_t global struct.
+*/
 void diagnostics_periodic(void);
 
 #endif // DIAGNOSTICS_H
