c671205ddf5494a764df126443298f039173c2d2
irving <irvingw@purdue.edu>
2026-09-24 13:54:56 -0400

    fine

--- firmware/cmake/dependencies.cmake
+++ firmware/cmake/dependencies.cmake
@@ -30,7 +30,7 @@ function(per_add_stm32_cmsis_target target family device device_dir)
     target_link_options(${target} INTERFACE ${cpu_flags})
     target_compile_options(${target} PRIVATE -fno-analyzer)
 
-    target_compile_definitions(${target} PUBLIC ${family} ${device})
+    target_compile_definitions(${target} PUBLIC ${family} ${device} HSE_VALUE=16000000U)
     target_include_directories(${target} PUBLIC
         "${CMSIS_CORE_DIR}"
         "${device_dir}/Include"
@@ -64,7 +64,6 @@ per_add_stm32_cmsis_target(
     CMSIS_F407 STM32F4xx STM32F407xx
     "${CMAKE_SOURCE_DIR}/external/cmsis-device-f4"
 )
-target_compile_definitions(CMSIS_F407 PUBLIC HSE_VALUE=16000000U)
 per_add_stm32_cmsis_target(
     CMSIS_G474 STM32G4xx STM32G474xx
     "${CMAKE_SOURCE_DIR}/external/cmsis-device-g4"
