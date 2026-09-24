90ac24c183a54c4b97dc28fa0c932b7c0ed5e3db
irving <irvingw@purdue.edu>
2026-09-24 15:01:24 -0400

    Fix F4 PLL Boost (#486)
    - run it back

--- firmware/cmake/dependencies.cmake
+++ firmware/cmake/dependencies.cmake
@@ -30,7 +30,7 @@ function(per_add_stm32_cmsis_target target family device device_dir)
     target_link_options(${target} INTERFACE ${cpu_flags})
     target_compile_options(${target} PRIVATE -fno-analyzer)
 
-    target_compile_definitions(${target} PUBLIC ${family} ${device})
+    target_compile_definitions(${target} PUBLIC ${family} ${device} HSE_VALUE=16000000U)
     target_include_directories(${target} PUBLIC
         "${CMSIS_CORE_DIR}"
         "${device_dir}/Include"
