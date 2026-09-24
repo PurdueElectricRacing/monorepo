5126a0b2b5084e18beb6481cc302d6ec9c6cad69
Kyle Yang <kyle.yang7728@gmail.com>
2026-09-24 16:30:55 -0400

    Added more tests for edge cases, min interval, and last saved ms

--- firmware/common/bangbang/tests/bangbang_test.cpp
+++ firmware/common/bangbang/tests/bangbang_test.cpp
@@ -17,14 +17,14 @@ constexpr float UPPER_BOUND = 50.0f;
 constexpr float LOWER_BOUND = 25.0f;
 constexpr uint32_t MIN_SWITCH_INTERVAL = 1000;
 
-bool turnedOn;
+bool turned_on;
 
 void onCallback() {
-    turnedOn = true;
+    turned_on = true;
 }
 
 void offCallback() {
-    turnedOn = false;
+    turned_on = false;
 }
 
 } // namespace
@@ -38,48 +38,49 @@ class BangbangTest : public ::testing::Test {
         controller.last_switch_ms = 0;
         controller.min_switch_interval = MIN_SWITCH_INTERVAL;
         controller.is_on = false;
-        turnedOn = false;
+        turned_on = false;
     }
 
     bangbang_t controller;
 };
 
-// Test that last saved ms works
+// TODO: Add messages for test cases for easier debugging
+// TODO: Check if tests are simplified to exactly what is to be tested
 
 // Test upper bounds
 TEST_F(BangbangTest, AboveUpperBoundTurnsOn) {
     bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
-    EXPECT_EQ(turnedOn, true);
+    EXPECT_EQ(turned_on, true);
     EXPECT_EQ(controller.is_on, true);
 }
 
 // Test lower bounds
 TEST_F(BangbangTest, BelowLowerBoundTurnsOff) {
     controller.is_on = true;
-    turnedOn = true;
-    ASSERT_EQ(turnedOn, true);
+    turned_on = true;
+    ASSERT_EQ(turned_on, true);
     ASSERT_EQ(controller.is_on, true);
 
-    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL);
+    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL+1);
     EXPECT_EQ(controller.is_on, false);
-    EXPECT_EQ(turnedOn, false);
+    EXPECT_EQ(turned_on, false);
 }
 
 // Test that min interval works
 TEST_F(BangbangTest, LessThanMinIntervalDoesNotSwitch) {
     bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL-1);
-    ASSERT_EQ(controller.is_on, false);
-    ASSERT_EQ(turnedOn, false);
+    EXPECT_EQ(controller.is_on, false);
+    EXPECT_EQ(turned_on, false);
 
     bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
-    ASSERT_EQ(controller.is_on, true);
-    ASSERT_EQ(turnedOn, true);
+    EXPECT_EQ(controller.is_on, true);
+    EXPECT_EQ(turned_on, true);
     
     controller.last_switch_ms = 0;
 
     bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL-1);
     EXPECT_EQ(controller.is_on, true);
-    EXPECT_EQ(turnedOn, true) << "controller called offCallback";
+    EXPECT_EQ(turned_on, true) << "controller called offCallback";
     // When I pass MIN_SWITCH_INTERVAL+1 and a valid upper bounds, do not reset last_switch_ms,
     // then pass MIN_SWITCH_INTERVAL-1 and a valid lower bounds, the controller will switch.
     // As time_since_last_int is an unsigned int, unsigned arithmetic overflow will wrap to 
@@ -87,7 +88,62 @@ TEST_F(BangbangTest, LessThanMinIntervalDoesNotSwitch) {
 }
 
 // Test edge cases for upper and lower bounds and switch interval
+TEST_F(BangbangTest, UpperBoundsTurnsOn) {
+    bangbang_update(&controller, UPPER_BOUND, MIN_SWITCH_INTERVAL+1);
+    EXPECT_EQ(turned_on, true);
+    EXPECT_EQ(controller.is_on, true);
+}
+
+TEST_F(BangbangTest, LowerBoundTurnsOff) {
+    controller.is_on = true;
+    turned_on = true;
+    ASSERT_EQ(turned_on, true);
+    ASSERT_EQ(controller.is_on, true);
+
+    bangbang_update(&controller, LOWER_BOUND, MIN_SWITCH_INTERVAL+1);
+    EXPECT_EQ(controller.is_on, false);
+    EXPECT_EQ(turned_on, false);
+}
+
+TEST_F(BangbangTest, ExactIntervalPasses) {
+    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL);
+    EXPECT_EQ(turned_on, true);
+    EXPECT_EQ(controller.is_on, true);
+
+    bangbang_update(&controller, LOWER_BOUND-1, 2*MIN_SWITCH_INTERVAL);
+    EXPECT_EQ(controller.is_on, false);
+    EXPECT_EQ(turned_on, false);
+}
+
+// Test last_saved_ms
+TEST_F(BangbangTest, LastSavedMsIsRetained) {
+    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
+    EXPECT_EQ(turned_on, true);
+    EXPECT_EQ(controller.is_on, true);
+    EXPECT_EQ(controller.last_switch_ms, MIN_SWITCH_INTERVAL+1);
+
+    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL+1);
+    EXPECT_EQ(controller.is_on, true);
+    EXPECT_EQ(turned_on, true);
+}
 
 // Test moving value (y = mx + b)
 
 // Test null pointers for functions
+TEST_F(BangbangTest, OnFunctionNullPointerDoesNotCall) {
+    // Give BangbangTest nullptr for on_func
+
+    // Try to call on_func using bangbang_update,
+    // should not run anything but is_on is true
+    // So should complete normally
+}
+
+TEST_F(BangbangTest, OffCallbackNullPointerDoesNotCall) {
+    // Give BangbangTest nullptr for off_func
+
+    // Set is_on and turned_on to true
+
+    // Try to call off_func using bangbang_update,
+    // should not run anything but is_on is false
+    // So should complete normally
+}
