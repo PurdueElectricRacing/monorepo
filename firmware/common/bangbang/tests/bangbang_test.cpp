ecf8df075d308a1c6578856e5c97a10a2e01db2a
Kyle Yang <kyle.yang7728@gmail.com>
2026-09-24 11:32:10 -0400

    Add two more tests

--- firmware/common/bangbang/tests/bangbang_test.cpp
+++ firmware/common/bangbang/tests/bangbang_test.cpp
@@ -15,6 +15,7 @@ namespace {
 
 constexpr float UPPER_BOUND = 50.0f;
 constexpr float LOWER_BOUND = 25.0f;
+constexpr uint32_t MIN_SWITCH_INTERVAL = 1000;
 
 bool turnedOn;
 
@@ -35,7 +36,7 @@ class BangbangTest : public ::testing::Test {
         controller.on_func = onCallback;
         controller.off_func = offCallback;
         controller.last_switch_ms = 0;
-        controller.min_switch_interval = 1000;
+        controller.min_switch_interval = MIN_SWITCH_INTERVAL;
         controller.is_on = false;
         turnedOn = false;
     }
@@ -47,24 +48,46 @@ class BangbangTest : public ::testing::Test {
 
 // Test upper bounds
 TEST_F(BangbangTest, AboveUpperBoundTurnsOn) {
-    bangbang_update(&controller, UPPER_BOUND+1, 2000);
+    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
     EXPECT_EQ(turnedOn, true);
-
-    // Turn the controller off
-
-    // Set time_since_last_switch to be above the min_switch_interval
-
-    // Pass a value above UPPER_BOUND using bangbang_update()
-
-    // Check if the controller turns on
+    EXPECT_EQ(controller.is_on, true);
 }
 
 // Test lower bounds
+TEST_F(BangbangTest, BelowLowerBoundTurnsOff) {
+    controller.is_on = true;
+    turnedOn = true;
+    ASSERT_EQ(turnedOn, true);
+    ASSERT_EQ(controller.is_on, true);
 
-// Test that min interval works (quick switching)
+    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL);
+    EXPECT_EQ(controller.is_on, false);
+    EXPECT_EQ(turnedOn, false);
+}
+
+// Test that min interval works
+TEST_F(BangbangTest, LessThanMinIntervalDoesNotSwitch) {
+    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL-1);
+    ASSERT_EQ(controller.is_on, false);
+    ASSERT_EQ(turnedOn, false);
+
+    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
+    ASSERT_EQ(controller.is_on, true);
+    ASSERT_EQ(turnedOn, true);
+    
+    controller.last_switch_ms = 0;
+
+    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL-1);
+    EXPECT_EQ(controller.is_on, true);
+    EXPECT_EQ(turnedOn, true) << "controller called offCallback";
+    // When I pass MIN_SWITCH_INTERVAL+1 and a valid upper bounds, do not reset last_switch_ms,
+    // then pass MIN_SWITCH_INTERVAL-1 and a valid lower bounds, the controller will switch.
+    // As time_since_last_int is an unsigned int, unsigned arithmetic overflow will wrap to 
+    // a large positive number. Unknown if this can be an issue.
+}
 
 // Test edge cases for upper and lower bounds and switch interval
 
-// Test moving value (y = mx + b) for hysteresis
+// Test moving value (y = mx + b)
 
 // Test null pointers for functions
