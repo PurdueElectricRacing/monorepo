199bae876af03de3199839794de93f47895768c9
Kyle Yang <kyle.yang7728@gmail.com>
2026-09-25 18:54:01 -0400

    Add final tests and clean up style and structure

--- firmware/common/bangbang/tests/bangbang_test.cpp
+++ firmware/common/bangbang/tests/bangbang_test.cpp
@@ -6,7 +6,6 @@
  */
 
 #include <gtest/gtest.h>
-#include <chrono>
 
 extern "C" {
 #include "bangbang.h"
@@ -45,132 +44,158 @@ class BangbangTest : public ::testing::Test {
     bangbang_t controller;
 };
 
-// TODO: Add messages for test cases for easier debugging
-// TODO: Check if tests are simplified to exactly what is to be tested
-
-// Test upper bounds
 TEST_F(BangbangTest, AboveUpperBoundTurnsOn) {
-    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
-    EXPECT_EQ(turned_on, true);
-    EXPECT_EQ(controller.is_on, true);
+    bangbang_update(&controller, UPPER_BOUND + 1, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_TRUE(controller.is_on);
+    EXPECT_TRUE(turned_on) << "on_func did not call";
 }
 
-// Test lower bounds
 TEST_F(BangbangTest, BelowLowerBoundTurnsOff) {
     controller.is_on = true;
     turned_on = true;
-    ASSERT_EQ(turned_on, true);
-    ASSERT_EQ(controller.is_on, true);
 
-    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL+1);
-    EXPECT_EQ(controller.is_on, false);
-    EXPECT_EQ(turned_on, false);
+    bangbang_update(&controller, LOWER_BOUND - 1, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_FALSE(controller.is_on);
+    EXPECT_FALSE(turned_on) << "off_func did not call";
 }
 
-// Test that min interval works
-TEST_F(BangbangTest, LessThanMinIntervalDoesNotSwitch) {
-    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL-1);
-    EXPECT_EQ(controller.is_on, false);
-    EXPECT_EQ(turned_on, false);
-
-    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
-    EXPECT_EQ(controller.is_on, true);
-    EXPECT_EQ(turned_on, true);
-    
-    controller.last_switch_ms = 0;
-
-    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL-1);
-    EXPECT_EQ(controller.is_on, true);
-    EXPECT_EQ(turned_on, true);
-}
-
-// Test edge cases for upper and lower bounds and switch interval
-TEST_F(BangbangTest, UpperBoundsTurnsOn) {
-    bangbang_update(&controller, UPPER_BOUND, MIN_SWITCH_INTERVAL+1);
-    EXPECT_EQ(turned_on, true);
-    EXPECT_EQ(controller.is_on, true);
+TEST_F(BangbangTest, UpperBoundTurnsOn) {
+    bangbang_update(&controller, UPPER_BOUND, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_TRUE(controller.is_on);
+    EXPECT_TRUE(turned_on) << "on_func did not call";
 }
 
 TEST_F(BangbangTest, LowerBoundTurnsOff) {
     controller.is_on = true;
     turned_on = true;
-    ASSERT_EQ(turned_on, true);
-    ASSERT_EQ(controller.is_on, true);
 
-    bangbang_update(&controller, LOWER_BOUND, MIN_SWITCH_INTERVAL+1);
-    EXPECT_EQ(controller.is_on, false);
-    EXPECT_EQ(turned_on, false);
+    bangbang_update(&controller, LOWER_BOUND, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_FALSE(controller.is_on);
+    EXPECT_FALSE(turned_on) << "off_func did not call";
+}
+
+TEST_F(BangbangTest, BetweenBoundsStaysOff) {
+    bangbang_update(&controller, UPPER_BOUND - 1, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_FALSE(controller.is_on);
+    EXPECT_FALSE(turned_on);
+}
+
+TEST_F(BangbangTest, BetweenBoundsStaysOn) {
+    controller.is_on = true;
+    turned_on = true;
+
+    bangbang_update(&controller, LOWER_BOUND + 1, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_TRUE(controller.is_on);
+    EXPECT_TRUE(turned_on);
+}
+
+TEST_F(BangbangTest, LessThanMinIntervalDoesNotSwitch) {
+    bangbang_update(&controller, UPPER_BOUND + 1, MIN_SWITCH_INTERVAL - 1);
+    EXPECT_FALSE(controller.is_on);
+    EXPECT_FALSE(turned_on);
+
+    controller.is_on = true;
+    turned_on = true;
+
+    bangbang_update(&controller, LOWER_BOUND - 1, MIN_SWITCH_INTERVAL - 1);
+    EXPECT_TRUE(controller.is_on);
+    EXPECT_TRUE(turned_on);
+}
+
+TEST_F(BangbangTest, LastSwitchMsUpdatesForSwitchOn) {
+    bangbang_update(&controller, UPPER_BOUND + 1, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_TRUE(controller.is_on);
+    EXPECT_TRUE(turned_on);
+    EXPECT_EQ(controller.last_switch_ms, MIN_SWITCH_INTERVAL + 1);
+
+    bangbang_update(&controller, LOWER_BOUND - 1, MIN_SWITCH_INTERVAL + 2);
+    EXPECT_TRUE(controller.is_on);
+    EXPECT_TRUE(turned_on);
+    EXPECT_EQ(controller.last_switch_ms, MIN_SWITCH_INTERVAL + 1) << "Second call updated last_switch_ms";
+}
+
+TEST_F(BangbangTest, LastSwitchMsUpdatesForSwitchOff) {
+    controller.is_on = true;
+    turned_on = true;
+
+    bangbang_update(&controller, LOWER_BOUND - 1, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_FALSE(controller.is_on);
+    EXPECT_FALSE(turned_on);
+    EXPECT_EQ(controller.last_switch_ms, MIN_SWITCH_INTERVAL + 1);
+
+    bangbang_update(&controller, UPPER_BOUND + 1, MIN_SWITCH_INTERVAL + 2);
+    EXPECT_FALSE(controller.is_on);
+    EXPECT_FALSE(turned_on);
+    EXPECT_EQ(controller.last_switch_ms, MIN_SWITCH_INTERVAL + 1) << "Second call updated last_switch_ms";
 }
 
 TEST_F(BangbangTest, ExactIntervalPasses) {
-    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL);
-    EXPECT_EQ(turned_on, true);
-    EXPECT_EQ(controller.is_on, true);
+    bangbang_update(&controller, UPPER_BOUND + 1, MIN_SWITCH_INTERVAL);
+    EXPECT_TRUE(turned_on);
+    EXPECT_TRUE(controller.is_on);
 
-    bangbang_update(&controller, LOWER_BOUND-1, 2*MIN_SWITCH_INTERVAL);
-    EXPECT_EQ(controller.is_on, false);
-    EXPECT_EQ(turned_on, false);
+    bangbang_update(&controller, LOWER_BOUND - 1, 2 * MIN_SWITCH_INTERVAL);
+    EXPECT_FALSE(controller.is_on);
+    EXPECT_FALSE(turned_on);
 }
 
-// Test last_saved_ms
-TEST_F(BangbangTest, LastSavedMsIsRetained) {
-    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
-    EXPECT_EQ(turned_on, true);
-    EXPECT_EQ(controller.is_on, true);
-    EXPECT_EQ(controller.last_switch_ms, MIN_SWITCH_INTERVAL+1);
+TEST_F(BangbangTest, OnFunctionCallsWithoutStateSwitch) {
+    controller.is_on = true;
 
-    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL+1);
-    EXPECT_EQ(controller.is_on, true);
-    EXPECT_EQ(turned_on, true);
+    bangbang_update(&controller, UPPER_BOUND - 1, MIN_SWITCH_INTERVAL - 1);
+    EXPECT_TRUE(turned_on) << "on_func not called";
 }
 
-// Test null pointers for functions
+TEST_F(BangbangTest, OffFunctionCallsWithoutStateSwitch) {
+    turned_on = true;
+
+    bangbang_update(&controller, LOWER_BOUND + 1, MIN_SWITCH_INTERVAL - 1);
+    EXPECT_FALSE(turned_on) << "off_func not called";
+}
+
 TEST_F(BangbangTest, OnFunctionNullPointerDoesNotCall) {
     controller.on_func = nullptr;
 
-    bangbang_update(&controller, UPPER_BOUND+1, MIN_SWITCH_INTERVAL+1);
-
-    EXPECT_EQ(controller.is_on, true);
+    bangbang_update(&controller, UPPER_BOUND + 1, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_TRUE(controller.is_on) << "Controller state does not change";
 }
 
-TEST_F(BangbangTest, OffCallbackNullPointerDoesNotCall) {
+TEST_F(BangbangTest, OffFunctionNullPointerDoesNotCall) {
     controller.off_func = nullptr;
     controller.is_on = true;
 
-    bangbang_update(&controller, LOWER_BOUND-1, MIN_SWITCH_INTERVAL+1);
-
-    EXPECT_EQ(controller.is_on, false);
+    bangbang_update(&controller, LOWER_BOUND - 1, MIN_SWITCH_INTERVAL + 1);
+    EXPECT_FALSE(controller.is_on) << "Controller state does not change";
 }
 
-// Test moving value (y = mx + b)
-TEST_F(BangbangTest, SlopeFunctionTest) {
+TEST_F(BangbangTest, LinearlyDecreasingInputFollowsHysteresis) {
     float value = 100.0f;
     uint32_t i;
 
     for (i = 0; i < 1000; i++) {
+        value = 100.0f - (0.04f * i);
         bangbang_update(&controller, value, i);
-        ASSERT_EQ(controller.is_on, false) << "turned on too early, value is " << value
-                                                    << " and timer is " << i
+        ASSERT_FALSE(controller.is_on) << "turned on too early, value is " << value \
+                                                    << " and timer is " << i \
                                                     << " and last_switch_ms is " << controller.last_switch_ms;
         
-        ASSERT_EQ(turned_on, false) << "on_func was called when it was not supposed to";
-        value -= 0.04;
+        ASSERT_FALSE(turned_on) << "on_func was called when it was not supposed to";
     }
 
     for (; i < 2000; i++) {
+        value = 100.0f - (0.04f * i);
         bangbang_update(&controller, value, i);
-        ASSERT_EQ(controller.is_on, true) << "turned off too early, value is " << value
-                                                   << ", timer is " << i
+        ASSERT_TRUE(controller.is_on) << "turned off too early, value is " << value \
+                                                   << ", timer is " << i \
                                                    << " and last_switch_ms is " << controller.last_switch_ms;
 
-        ASSERT_EQ(turned_on, true) << "off_func was called when it was not supposed to";
-        value -= 0.04;
+        ASSERT_TRUE(turned_on) << "off_func was called when it was not supposed to";
     }
 
     bangbang_update(&controller, value, i);
-    EXPECT_EQ(controller.is_on, false) << "failed last switch, value is " << value
-                                                << " and timer is " << i
+    EXPECT_FALSE(controller.is_on) << "failed last switch, value is " << value \
+                                                << " and timer is " << i \
                                                 << " and last_switch_ms is " << controller.last_switch_ms;
     
-    EXPECT_EQ(turned_on, false) << "on_func was called when it was not supposed to";
+    EXPECT_FALSE(turned_on) << "on_func was called when it was not supposed to";
 }
